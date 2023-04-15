5602105b5df5792ae72284f667400a1140605070
GravityProto <82423432+GravityProto@users.noreply.github.com>
2023-04-14 21:05:59 -0300

    Add files via upload

---
+++ triangulo/questao3.js
@@ -0,0 +1,4 @@
+let base = parseFloat(prompt("Digite a base do retangulo"))
+let altura = parseFloat(prompt("Digite a altura do retangulo"))
+alert(`A area do seu retangulo é: ${base * altura} \n 
+o perimetro do seu retangulo é: ${(base + altura)* 2}`)
